84c85fa8125b08891fb8a3d8d7f0ed7ca84811b9
fallzerr <naufaldest@gmail.com>
2026-09-23 22:36:42 +0700

    Test modul

---
+++ Modul1/main.py
@@ -0,0 +1,33 @@
+# nama variabel
+a = 10
+b = 5  
+
+# print("Hasil Penjumlahan (a + b) =", a + b)  
+# print("Hasil Pengurangan (a - b) =", a - b)  
+# print("Hasil Perkalian (a * b) =", a * b)  
+# print("Hasil Pangkat  (a ** b) =", a ** b)  
+# print("Hasil Pembagian (a / b) =", a / b)  
+# print("Hasil Pembagian Bulat (a // b) =", a // b)
+# print("Hasil Sisa Bagi (a % b)=", a % b)
+  
+kursi_204 = 30  
+kursi_308 = 20  
+jumlah = kursi_204 + kursi_308  
+# print("jumlah kursi_204 + kursi_308 = ",jumlah)  
+
+nama = "Agus pergi ke pasar" 
+# print(nama) 
+
+# contoh tipe data 
+
+# Integer  
+nilai_integer = 25  
+# print("Nilai Integer:", nilai_integer)  
+
+# Float  
+nilai_float = 3.14  
+# print("Nilai Float:", nilai_float)  
+  
+# String  
+nilai_string = "Hello Word"  
+# print("Nilai String:", nilai_string)  
